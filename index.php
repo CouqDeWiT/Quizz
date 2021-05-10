d325d40dd1b08871be978f0549e95e3d58ca2eab
duonggiakhanhb <duonggiakhanhb@gmail.com>
2021-05-10 12:57:07 +0700

    index Page

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Only CSS: Shooting Star</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Anton'><link rel="stylesheet" href="./css/styleI.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="main"></div>
+<div class="main night">
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+  <div class="shooting_star"></div>
+</div>
+<div class="main text">
+  <h1 >Quizz</h1>
+  <h2>by Adam</h2>
+</div>
+<!-- partial -->
+<script>
+    setTimeout(() => {
+        document.location.href = '/indexx.php';
+    }, 3000);
+
+</script>
+</body>
+</html>
+
