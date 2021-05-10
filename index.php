51d8ba13a8c6251a567e964c871d995ec7da1e8d
duonggiakhanhb <duonggiakhanhb@gmail.com>
2021-05-10 11:52:46 +0700

    css update

--- index.php
+++ index.php
@@ -10,14 +10,15 @@ if(isset($_POST['next'])){
 function questChoice($index){
     $_SESSION['current']=$index;
     return ("
+<div class='questChoice'>
        <div class='Question' name = 'quest-$index'>{$_SESSION['data'][$index]['quest']}</div>
-        <button class='next' name='next'></button>
        <div class='grid-containerQuest'>
        <p id='A' name='btn-c-A' class = 'btn-c'>{$_SESSION["data"][$index]['inputA']}</p>
        <p id='B' name='btn-c-B' class = 'btn-c'>{$_SESSION["data"][$index]['inputB']}</p>
        <p id='C' name='btn-c-C' class = 'btn-c'>{$_SESSION["data"][$index]['inputC']}</p>
        <p id='D' name='btn-c-D' class = 'btn-c'>{$_SESSION["data"][$index]['inputD']}</p>
 
+</div>
 </div>
     ");
 }
@@ -28,7 +29,9 @@ function questChoice($index){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel = "stylesheet" type="text/css" href = 'css/index.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    <link rel = "stylesheet" type="text/css" href = 'css/index.css'>
   <meta charset="UTF-8">
   <title>Quizz Input</title>
 
@@ -38,13 +41,16 @@ function questChoice($index){
     <button type="submit" class="nhap">Nhap</button>
 </form>
 <form method="post" class="grid-containerQ">
-<?php
-foreach ($_SESSION['data'] as $index => $datum){
-    echo "<button class='Quest' name='btn-$index'>". $index+1 ."</button>";
-}
-?>
+    <div class="btn-i">
+        <?php
+        foreach ($_SESSION['data'] as $index => $datum){
+            echo "<button class='Quest' name='btn-$index'>". $index+1 ."</button>";
+        }
+        ?>
+    </div>
+    <div class="btn"><button class='next' name='next'></button></div>
 </form>
-<form action="" method="post">
+<form action="" method="post" >
 <?php
     foreach ($_SESSION['data'] as $index => $datum){
         if(isset($_POST["btn-$index"])){
@@ -78,11 +84,12 @@ else echo "<script type='text/javascript'>alert('wrong');</script>";
     $('#D').click(function (){check('D')});
     function check(key){
         let value = '#' + key;
-        if(key ==checkCorrect){
-            $(value).css("background", "green");
+        if(key == checkCorrect){
+            $(value).css("background", "rgba(124,252,0,0.61)");
         }
-        else $(value).css("background", "red");
+        else $(value).css("background", "#ff1a1a");
     }
+    $("button[name=btn-<?php echo $_SESSION['current']?>]").css("border-color", "#7AC40EFF");
 </script>
 </body>
 </html>
